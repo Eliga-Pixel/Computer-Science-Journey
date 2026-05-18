9b33a69c2fe474906312576b8ab5fada5460af56
Eliga-Pixel <amoneliga9@gmail.com>
2026-05-18 07:17:28 +0200

    Create if_statements.py
    Add if_statements.py

---
+++ Python/if_statements.py
@@ -0,0 +1,13 @@
+# if_statements.py
+# Lesson: Conditional statements
+
+# Ask for a number
+num = int(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
